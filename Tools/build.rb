38c0fbe7b4db1e88383e3f0f0225f83d4aebcc02
Patrick Ellis <patrick@soundcloud.com>
2012-08-17 16:06:31 +0200

    print the error message after build failures

--- Tools/build.rb
+++ Tools/build.rb
@@ -47,12 +47,12 @@ Open3.popen3("xcodebuild -project Soundflower.xcodeproj -target SoundflowerDrive
 end
 
 
-#if /BUILD SUCCEEDED/.match(out)
-#  puts "    BUILD SUCCEEDED"
-#  puts `ruby #{@svn_root}/Tools/load.rb`
-#else
-#  puts "    BUILD FAILED"
-#end
+if /BUILD SUCCEEDED/.match(out)
+ puts "    BUILD SUCCEEDED"
+else
+ puts "    BUILD FAILED"
+ puts "      #{err}"
+end
 
 
 ###################################################################
@@ -70,6 +70,7 @@ if /BUILD SUCCEEDED/.match(out)
   puts "    BUILD SUCCEEDED"
 else
   puts "    BUILD FAILED"
+  puts "      #{err}"
 end
 
 
