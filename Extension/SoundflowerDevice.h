24d6c7ce93d00f940c35cc16756404a0364585d3
Patrick Ellis <patrick@soundcloud.com>
2012-09-02 00:09:11 +0200

    remove last references to "cycling74" from plists

--- Extension/SoundflowerDevice.h
+++ Extension/SoundflowerDevice.h
@@ -8,12 +8,12 @@
     modify it under the terms of the GNU General Public License
     as published by the Free Software Foundation; either version 2
     of the License, or (at your option) any later version.
-    
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-    
+
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -33,7 +33,7 @@
 #define SAMPLE_RATES_KEY				"SampleRates"
 #define SEPARATE_STREAM_BUFFERS_KEY		"SeparateStreamBuffers"
 #define SEPARATE_INPUT_BUFFERS_KEY		"SeparateInputBuffers"
-#define SoundflowerDevice				com_cycling74_driver_SoundflowerDevice
+#define SoundflowerDevice				com_wavtap_driver_WavTapDevice
 
 class SoundflowerEngine;
 
@@ -41,13 +41,13 @@ class SoundflowerDevice : public IOAudioDevice
 {
     OSDeclareDefaultStructors(SoundflowerDevice)
     friend class SoundflowerEngine;
-    
+
 	// class members
-	
+
     static const SInt32 kVolumeMax;
     static const SInt32 kGainMax;
 
-	
+
 	// instance members
 
     SInt32 mVolume[NUM_CHANS+1];
@@ -55,25 +55,25 @@ class SoundflowerDevice : public IOAudioDevice
     SInt32 mMuteIn[NUM_CHANS+1];
     SInt32 mGain[NUM_CHANS+1];
 
-	
+
 	// methods
-	
+
     virtual bool initHardware(IOService *provider);
     virtual bool createAudioEngines();
     virtual bool initControls(SoundflowerEngine *audioEngine);
-    
+
     static  IOReturn volumeChangeHandler(IOService *target, IOAudioControl *volumeControl, SInt32 oldValue, SInt32 newValue);
     virtual IOReturn volumeChanged(IOAudioControl *volumeControl, SInt32 oldValue, SInt32 newValue);
-    
+
     static  IOReturn outputMuteChangeHandler(IOService *target, IOAudioControl *muteControl, SInt32 oldValue, SInt32 newValue);
     virtual IOReturn outputMuteChanged(IOAudioControl *muteControl, SInt32 oldValue, SInt32 newValue);
 
     static  IOReturn gainChangeHandler(IOService *target, IOAudioControl *gainControl, SInt32 oldValue, SInt32 newValue);
     virtual IOReturn gainChanged(IOAudioControl *gainControl, SInt32 oldValue, SInt32 newValue);
-    
+
     static  IOReturn inputMuteChangeHandler(IOService *target, IOAudioControl *muteControl, SInt32 oldValue, SInt32 newValue);
     virtual IOReturn inputMuteChanged(IOAudioControl *muteControl, SInt32 oldValue, SInt32 newValue);
-    
+
 };
 
 #endif // _SAMPLEAUDIODEVICE_H
