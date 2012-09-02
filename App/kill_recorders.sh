a9713800a994294a8549f7f6da5f8ce9b04e7c75
Patrick Ellis <patrick@soundcloud.com>
2012-09-02 11:39:08 +0200

    Actually kill the recorder process #bugfixin

--- App/kill_recorders.sh
+++ App/kill_recorders.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 recorder_process='/Applications/WavTap.app/Contents/SharedSupport/sox'
-ps -axo pid,command,args | grep $recorder_process | grep -v grep | awk '{ print $1 }' | kill -SIGINT
+ps -axo pid,command,args | grep $recorder_process | grep -v grep | awk '{ print $1 }' | xargs kill -s SIGINT
+
