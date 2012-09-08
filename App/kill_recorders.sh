8a134a097b09607b0f2dd1ea04cca095d1d04960
Patrick Ellis <patrick@soundcloud.com>
2012-09-08 19:56:29 +0200

    search for sox relative to current directory
    - instead of hardcoding the path to /Applications

--- App/kill_recorders.sh
+++ App/kill_recorders.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-recorder_process='/Applications/WavTap.app/Contents/SharedSupport/sox'
+recorder_process='WavTap.app/Contents/SharedSupport/sox'
 ps -axo pid,command,args | grep $recorder_process | grep -v grep | awk '{ print $1 }' | xargs kill -s SIGINT
-
