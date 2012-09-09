1641e4782b4f1c49b21cf73822e4d13bc2171a65
Patrick Ellis <patrick@soundcloud.com>
2012-09-09 19:34:04 +0200

    WS & formatting consistency

--- Extension/WavTapDevice.h
+++ Extension/WavTapDevice.h
@@ -3,56 +3,39 @@
 
 #include <IOKit/audio/IOAudioDevice.h>
 
-#define AUDIO_ENGINES_KEY				"AudioEngines"
-#define DESCRIPTION_KEY					"Description"
-#define BLOCK_SIZE_KEY					"BlockSize"
-#define NUM_BLOCKS_KEY					"NumBlocks"
-#define NUM_STREAMS_KEY					"NumStreams"
-#define FORMATS_KEY						"Formats"
-#define SAMPLE_RATES_KEY				"SampleRates"
-#define SEPARATE_STREAM_BUFFERS_KEY		"SeparateStreamBuffers"
-#define SEPARATE_INPUT_BUFFERS_KEY		"SeparateInputBuffers"
-#define WavTapDevice				com_wavtap_driver_WavTapDevice
+#define AUDIO_ENGINES_KEY "AudioEngines"
+#define DESCRIPTION_KEY "Description"
+#define BLOCK_SIZE_KEY "BlockSize"
+#define NUM_BLOCKS_KEY "NumBlocks"
+#define NUM_STREAMS_KEY "NumStreams"
+#define FORMATS_KEY "Formats"
+#define SAMPLE_RATES_KEY "SampleRates"
+#define SEPARATE_STREAM_BUFFERS_KEY "SeparateStreamBuffers"
+#define SEPARATE_INPUT_BUFFERS_KEY "SeparateInputBuffers"
+#define WavTapDevice com_wavtap_driver_WavTapDevice
 
 class WavTapEngine;
 
-class WavTapDevice : public IOAudioDevice
-{
-    OSDeclareDefaultStructors(WavTapDevice)
-    friend class WavTapEngine;
-
-	// class members
-
-    static const SInt32 kVolumeMax;
-    static const SInt32 kGainMax;
-
-
-	// instance members
-
-    SInt32 mVolume[NUM_CHANS+1];
-    SInt32 mMuteOut[NUM_CHANS+1];
-    SInt32 mMuteIn[NUM_CHANS+1];
-    SInt32 mGain[NUM_CHANS+1];
-
-
-	// methods
-
-    virtual bool initHardware(IOService *provider);
-    virtual bool createAudioEngines();
-    virtual bool initControls(WavTapEngine *audioEngine);
-
-    static  IOReturn volumeChangeHandler(IOService *target, IOAudioControl *volumeControl, SInt32 oldValue, SInt32 newValue);
-    virtual IOReturn volumeChanged(IOAudioControl *volumeControl, SInt32 oldValue, SInt32 newValue);
-
-    static  IOReturn outputMuteChangeHandler(IOService *target, IOAudioControl *muteControl, SInt32 oldValue, SInt32 newValue);
-    virtual IOReturn outputMuteChanged(IOAudioControl *muteControl, SInt32 oldValue, SInt32 newValue);
-
-    static  IOReturn gainChangeHandler(IOService *target, IOAudioControl *gainControl, SInt32 oldValue, SInt32 newValue);
-    virtual IOReturn gainChanged(IOAudioControl *gainControl, SInt32 oldValue, SInt32 newValue);
-
-    static  IOReturn inputMuteChangeHandler(IOService *target, IOAudioControl *muteControl, SInt32 oldValue, SInt32 newValue);
-    virtual IOReturn inputMuteChanged(IOAudioControl *muteControl, SInt32 oldValue, SInt32 newValue);
-
+class WavTapDevice : public IOAudioDevice {
+  OSDeclareDefaultStructors(WavTapDevice);
+  friend class WavTapEngine;
+  static const SInt32 kVolumeMax;
+  static const SInt32 kGainMax;
+  SInt32 mVolume[NUM_CHANS+1];
+  SInt32 mMuteOut[NUM_CHANS+1];
+  SInt32 mMuteIn[NUM_CHANS+1];
+  SInt32 mGain[NUM_CHANS+1];
+  virtual bool initHardware(IOService *provider);
+  virtual bool createAudioEngines();
+  virtual bool initControls(WavTapEngine *audioEngine);
+  static IOReturn volumeChangeHandler(IOService *target, IOAudioControl *volumeControl, SInt32 oldValue, SInt32 newValue);
+  virtual IOReturn volumeChanged(IOAudioControl *volumeControl, SInt32 oldValue, SInt32 newValue);
+  static IOReturn outputMuteChangeHandler(IOService *target, IOAudioControl *muteControl, SInt32 oldValue, SInt32 newValue);
+  virtual IOReturn outputMuteChanged(IOAudioControl *muteControl, SInt32 oldValue, SInt32 newValue);
+  static IOReturn gainChangeHandler(IOService *target, IOAudioControl *gainControl, SInt32 oldValue, SInt32 newValue);
+  virtual IOReturn gainChanged(IOAudioControl *gainControl, SInt32 oldValue, SInt32 newValue);
+  static IOReturn inputMuteChangeHandler(IOService *target, IOAudioControl *muteControl, SInt32 oldValue, SInt32 newValue);
+  virtual IOReturn inputMuteChanged(IOAudioControl *muteControl, SInt32 oldValue, SInt32 newValue);
 };
 
-#endif // _SAMPLEAUDIODEVICE_H
+#endif
--- Extension/WavTapEngine.h
+++ Extension/WavTapEngine.h
@@ -4,56 +4,34 @@
 #include <IOKit/audio/IOAudioEngine.h>
 #include "WavTapDevice.h"
 
-
-class WavTapEngine : public IOAudioEngine
-{
-    OSDeclareDefaultStructors(WavTapEngine)
-
-	UInt32				mBufferSize;
-	void*				mBuffer;				// input/output buffer
-    float*				mThruBuffer;			// intermediate buffer to pass in-->out
-
-	IOAudioStream*		outputStream;
-	IOAudioStream*		inputStream;
-
-	UInt32				mLastValidSampleFrame;
-
-    IOTimerEventSource*	timerEventSource;
-
-    UInt32				blockSize;				// In sample frames -- fixed, as defined in the Info.plist (e.g. 8192)
-    UInt32				numBlocks;
-    UInt32				currentBlock;
-
-    UInt64				blockTimeoutNS;
-    UInt64				nextTime;				// the estimated time the timer will fire next
-
-    bool				duringHardwareInit;
-
-	float             logTable[100] ; // Lookup for logarithmic volume scaling.
-
-
+class WavTapEngine : public IOAudioEngine {
+  OSDeclareDefaultStructors(WavTapEngine);
+	UInt32 mBufferSize;
+	void *mBuffer;
+  float *mThruBuffer;
+	IOAudioStream *outputStream;
+	IOAudioStream *inputStream;
+	UInt32 mLastValidSampleFrame;
+  IOTimerEventSource*	timerEventSource;
+  UInt32 blockSize;
+  UInt32 numBlocks;
+  UInt32 currentBlock;
+  UInt64 blockTimeoutNS;
+  UInt64 nextTime;
+  bool duringHardwareInit;
+	float logTable[100];
 public:
-
-    virtual bool init(OSDictionary *properties);
-    virtual void free();
-
-    virtual bool initHardware(IOService *provider);
-
-    virtual bool createAudioStreams(IOAudioSampleRate *initialSampleRate);
-
-    virtual IOReturn performAudioEngineStart();
-    virtual IOReturn performAudioEngineStop();
-
-    virtual UInt32 getCurrentSampleFrame();
-
-    virtual IOReturn performFormatChange(IOAudioStream *audioStream, const IOAudioStreamFormat *newFormat, const IOAudioSampleRate *newSampleRate);
-
-    virtual IOReturn clipOutputSamples(const void *mixBuf, void *sampleBuf, UInt32 firstSampleFrame, UInt32 numSampleFrames, const IOAudioStreamFormat *streamFormat, IOAudioStream *audioStream);
-    virtual IOReturn convertInputSamples(const void *sampleBuf, void *destBuf, UInt32 firstSampleFrame, UInt32 numSampleFrames, const IOAudioStreamFormat *streamFormat, IOAudioStream *audioStream);
-
-    static void ourTimerFired(OSObject *target, IOTimerEventSource *sender);
-
+  virtual bool init(OSDictionary *properties);
+  virtual void free();
+  virtual bool initHardware(IOService *provider);
+  virtual bool createAudioStreams(IOAudioSampleRate *initialSampleRate);
+  virtual IOReturn performAudioEngineStart();
+  virtual IOReturn performAudioEngineStop();
+  virtual UInt32 getCurrentSampleFrame();
+  virtual IOReturn performFormatChange(IOAudioStream *audioStream, const IOAudioStreamFormat *newFormat, const IOAudioSampleRate *newSampleRate);
+  virtual IOReturn clipOutputSamples(const void *mixBuf, void *sampleBuf, UInt32 firstSampleFrame, UInt32 numSampleFrames, const IOAudioStreamFormat *streamFormat, IOAudioStream *audioStream);
+  virtual IOReturn convertInputSamples(const void *sampleBuf, void *destBuf, UInt32 firstSampleFrame, UInt32 numSampleFrames, const IOAudioStreamFormat *streamFormat, IOAudioStream *audioStream);
+  static void ourTimerFired(OSObject *target, IOTimerEventSource *sender);
 };
 
-
-#endif /* _WavTapENGINE_H */
+#endif
