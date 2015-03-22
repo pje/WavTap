41f0af48403996ee37b13c3c0f793154ee7980b5
Patrick Ellis <patrick@soundcloud.com>
2015-03-22 17:21:31 -0400

    Refactor Installer pre- and post-install scripts

--- Installer/kext_postinstall.sh
+++ Installer/kext_postinstall.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 PRODUCT_NAME=WavTap
+INSTALL_DESTINATION=/System/Library/Extensions
 
-sudo chmod -R 700 /System/Library/Extensions/$PRODUCT_NAME.kext
-sudo chown -R root:wheel /System/Library/Extensions/$PRODUCT_NAME.kext
-# sudo kextload -v /System/Library/Extensions/$PRODUCT_NAME.kext
-# sudo kextutil /System/Library/Extensions/$PRODUCT_NAME.kext
+sudo chmod -R 700 "$INSTALL_DESTINATION/$PRODUCT_NAME.kext"
+sudo chown -R root:wheel "$INSTALL_DESTINATION/$PRODUCT_NAME.kext"
+# sudo kextload -v "$INSTALL_DESTINATION/$PRODUCT_NAME.kext"
+# sudo kextutil "$INSTALL_DESTINATION/$PRODUCT_NAME.kext"
--- Installer/kext_preinstall.sh
+++ Installer/kext_preinstall.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 PRODUCT_NAME=WavTap
+INSTALL_DESTINATION=/System/Library/Extensions
 
-# if [[ "$(kextstat | grep $PRODUCT_NAME | grep -v grep)" ]]; then sudo kextunload /System/Library/Extensions/$PRODUCT_NAME.kext; fi
-sudo rm -rf /System/Library/Extensions/$PRODUCT_NAME.kext
+# if [[ "$(kextstat | grep $PRODUCT_NAME | grep -v grep)" ]]; then sudo kextunload "$INSTALL_DESTINATION/$PRODUCT_NAME.kext"; fi
+sudo rm -rf "$INSTALL_DESTINATION/$PRODUCT_NAME.kext"
 sudo rm -rf /Library/Receipts/$PRODUCT_NAME*
 sudo rm -rf /var/db/receipts/*$PRODUCT_NAME.*
